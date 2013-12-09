9a95dc76be5cdce1fd526475dcc6c11669bc65ef
gongpengjun <frank.gongpengjun@gmail.com>
2013-12-09 14:19:15 +0800

    应用在前台时禁止屏幕变暗

--- NameCard/GPJAppDelegate.m
+++ NameCard/GPJAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    application.idleTimerDisabled = YES;
     return YES;
 }
 							
@@ -24,7 +25,8 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    application.idleTimerDisabled = NO;
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
@@ -36,6 +38,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    application.idleTimerDisabled = YES;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
